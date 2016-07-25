30ad3e805ee49c0a459aad1ad9f110bcd9cea4ff
timgrossmann <t.grossmann1994@web.de>
2016-07-25 14:12:02 +0200

    handling of different screen sizes

--- examples/pong/server/index.js
+++ examples/pong/server/index.js
@@ -4,10 +4,14 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const swip = require('../../../src/index.js');
 
+app.use(express.static(__dirname + './../static'));
+
 swip(io);
 
+io.on('connection', (socket) => {
+  socket.on('init', (data) => {
+    console.log(data);
+  });
+});
+
 server.listen(3000);
-
-app.use(express.static(__dirname + '../static'));
-
-console.log('running server on http://localhost:3000');
