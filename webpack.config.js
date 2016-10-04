13c016cae5d98185433def9ffb50877db57209e6
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-10-04 21:13:13 +0200

    Restructure client

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel', // 'babel-loader' is also a legal name to reference
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
     ],
   },
 };
